672f63a07b7b5f1c00b379636fd27e39ac69ed6b
VT-DELQ <63230495+VT-DELQ@users.noreply.github.com>
2020-05-15 11:37:59 +0200

    Add files via upload

---
+++ ESP_HACK.cpp
@@ -0,0 +1,107 @@
+/*
+ *
+ *
+ *    
+ *              _____                    _____                    _____                                   
+ *             /\    \                  /\    \                  /\    \                                  
+ *            /::\    \                /::\    \                /::\    \                                 
+ *           /::::\    \              /::::\    \              /::::\    \                                
+ *          /::::::\    \            /::::::\    \            /::::::\    \                               
+ *         /:::/\:::\    \          /:::/\:::\    \          /:::/\:::\    \                              
+ *        /:::/__\:::\    \        /:::/__\:::\    \        /:::/__\:::\    \                             
+ *       /::::\   \:::\    \       \:::\   \:::\    \      /::::\   \:::\    \                            
+ *      /::::::\   \:::\    \    ___\:::\   \:::\    \    /::::::\   \:::\    \                           
+ *     /:::/\:::\   \:::\    \  /\   \:::\   \:::\    \  /:::/\:::\   \:::\____\                          
+ *    /:::/__\:::\   \:::\____\/::\   \:::\   \:::\____\/:::/  \:::\   \:::|    |                         
+ *    \:::\   \:::\   \::/    /\:::\   \:::\   \::/    /\::/    \:::\  /:::|____|                         
+ *     \:::\   \:::\   \/____/  \:::\   \:::\   \/____/  \/_____/\:::\/:::/    /                          
+ *      \:::\   \:::\    \       \:::\   \:::\    \               \::::::/    /                           
+ *       \:::\   \:::\____\       \:::\   \:::\____\               \::::/    /                            
+ *        \:::\   \::/    /        \:::\  /:::/    /                \::/____/                             
+ *         \:::\   \/____/          \:::\/:::/    /                  ~~                                   
+ *          \:::\    \               \::::::/    /                                                        
+ *           \:::\____\               \::::/    /                                                         
+ *            \::/    /                \::/    /                                                          
+ *             \/____/                  \/____/                                                           
+ *                                        
+ *
+ *
+ *        +++++ >>>>------------> Extra Sensory Perception <------------<<<< +++++
+ *
+ *                                                                
+ *          _____                    _____                    _____                    _____          
+ *         /\    \                  /\    \                  /\    \                  /\    \         
+ *        /::\____\                /::\    \                /::\    \                /::\____\        
+ *       /:::/    /               /::::\    \              /::::\    \              /:::/    /        
+ *      /:::/    /               /::::::\    \            /::::::\    \            /:::/    /         
+ *     /:::/    /               /:::/\:::\    \          /:::/\:::\    \          /:::/    /          
+ *    /:::/____/               /:::/__\:::\    \        /:::/  \:::\    \        /:::/____/           
+ *   /::::\    \              /::::\   \:::\    \      /:::/    \:::\    \      /::::\    \           
+ *  /::::::\    \   _____    /::::::\   \:::\    \    /:::/    / \:::\    \    /::::::\____\________  
+ * /:::/\:::\    \ /\    \  /:::/\:::\   \:::\    \  /:::/    /   \:::\    \  /:::/\:::::::::::\    \ 
+ */:::/  \:::\    /::\____\/:::/  \:::\   \:::\____\/:::/____/     \:::\____\/:::/  |:::::::::::\____\
+ *\::/    \:::\  /:::/    /\::/    \:::\  /:::/    /\:::\    \      \::/    /\::/   |::|~~~|~~~~~     
+ * \/____/ \:::\/:::/    /  \/____/ \:::\/:::/    /  \:::\    \      \/____/  \/____|::|   |          
+ *          \::::::/    /            \::::::/    /    \:::\    \                    |::|   |          
+ *           \::::/    /              \::::/    /      \:::\    \                   |::|   |          
+ *           /:::/    /               /:::/    /        \:::\    \                  |::|   |          
+ *          /:::/    /               /:::/    /          \:::\    \                 |::|   |          
+ *         /:::/    /               /:::/    /            \:::\    \                |::|   |          
+ *        /:::/    /               /:::/    /              \:::\____\               \::|   |          
+ *        \::/    /                \::/    /                \::/    /                \:|   |          
+ *         \/____/                  \/____/                  \/____/                  \|___|                                                           
+ *
+ *
+ *
+ *     ESP (Extra Sensory Perception) HACK FOR CONSOLE:
+ *                                                                                  
+ *     Wanna polish your battle skills with sharper vision and perception? Get a better glimpse 
+ *     of you enemies hiding in the shadows with this completely undetectable hack for console. 
+ *     How? By playing on a Samsung QLED TV.
+ *
+ *     Yup, with QLED there’s no need for cheats, since its technology will improve your gaming 
+ *     in a totally legit way.
+ *
+ *     QLED’s screen brightness can exceed other TV technologies, and depending on  
+ *     model reach up to 4000 nits. Selected models also feature anti-reflection thanks  
+ *     to the light-absorbing properties between the inner and outer film of the liquid 
+ *     crystal display. This means that deeper blacks are maintained and rich colours  
+ *     are displaced on the screen without reflecting the light back. In addition, 
+ *     QLED’s Dynamic Black Equalizer will help you discover both details and opponents 
+ *     even in the darkest areas. 
+ *  
+ *     Better vision, easier targets. No need for peering or closed curtains – QLED will 
+ *     help you discover your enemies and take them down.
+ *     
+ *
+ *         
+ *      ----------------------------------------------------------                                                                
+ *     |                                                          |
+ *     |                                                          |
+ *     |                                                          |
+ *     |                                                          |
+ *     |                 Put an end to detail loss                |	
+ *     |                                                          |
+ *     |                                                          |
+ *     |                and improve your game with                |
+ *     |                                                          |
+ *     |                                                          |
+ *     |               --->  Samsung QLED TV  <---                |
+ *     |                                                          |
+ *     |                                                          |  
+ *     |                                                          |
+ *     |                                                          |  
+ *      ----------------------------------------------------------
+ *              /\                                    /\      
+ *
+ *        https://www.samsung.com/se/tvs/all-tvs/?qled-4k+qled-8k 
+ *
+ *
+ *                                                                       
+ */
+
+
+
+
+
+
